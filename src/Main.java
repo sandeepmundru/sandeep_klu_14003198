091603bedcd9760e93e32331ecf398ce6f7c152f
sandeepmundru <31949629+sandeepmundru@users.noreply.github.com>
2017-09-14 13:27:39 +0530

    Object Creation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Student sc = new Student();
+		Student sc = new Student(System.in);
+        StudentGroup sg = new StudentGroup(System.in);
+        StudentArrayOperations sa = new StudentArrayOperation(System.in);
 
+        sc.id(3455);
+        sc.fullname(Sandeep Mundru);
+        sc.birthDate(1996-02-18);
+        sc.avgMarks(56.3);
 	}
 
 }
--- src/StudentArrayOperation.java
+++ src/StudentArrayOperation.java
@@ -1,3 +1,4 @@
+
 import java.util.Date;
 
 /**
@@ -8,23 +9,23 @@ public interface StudentArrayOperation {
 
 	/**
 	 * Returns the array of students
-	 * 
+	 *
 	 * @return the array of students
-	 * 
+	 *
 	 */
 	Student[] getStudents();
-	
+
 
 	/**
 	 * Sets the array of students
 	 * if students == null method should throw IllegalArgumentException
 	 *
 	 * @param students
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void setStudents(Student[] students);
-	
+
 
 	/**
 	 * if index lower than 0 or index higher/equal students.length method should
@@ -34,7 +35,7 @@ public interface StudentArrayOperation {
 	 */
 	Student getStudent(int index);
 
-	
+
 	/**
 	 * Replaces the element at the specified position in this array with the
 	 * specified element. if student == null method should throw
@@ -45,13 +46,13 @@ public interface StudentArrayOperation {
 	 *            the student to be stored at the specified position
 	 * @param index
 	 *            the index of the element to replace
-	 * 
-	 * 
+	 *
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void setStudent(Student student, int index);
 
-	
+
 	/**
 	 * Appends the specified element to the specified position of this array if
 	 * student == null method should throw IllegalArgumentException if index
@@ -63,23 +64,23 @@ public interface StudentArrayOperation {
 	 *            position
 	 * @param index
 	 *            the specified position
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void add(Student student, int index);
 
-	
+
 	/**
 	 * Inserts the specified element at the beginning of this array if student
 	 * == null method should throw IllegalArgumentException
 	 *
 	 * @param student
 	 *            the element to add
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void addFirst(Student student);
-	
+
 
 	/**
 	 * Appends the specified element to the end of this array. if student ==
@@ -87,11 +88,11 @@ public interface StudentArrayOperation {
 	 *
 	 * @param student
 	 *            the element to add
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void addLast(Student student);
-	
+
 
 	/**
 	 * Removes the element at the specified position in this array if index
@@ -100,11 +101,11 @@ public interface StudentArrayOperation {
 	 *
 	 * @param index
 	 *            the index of the element to be removed
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void remove(int index);
-	
+
 
 	/**
 	 * Removes the first occurrence of the specified element from this array, if
@@ -115,12 +116,12 @@ public interface StudentArrayOperation {
 	 *
 	 * @param student
 	 *            the element to remove
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void remove(Student student);
 
-	
+
 	/**
 	 * Removes all elements from the specified index (except the element with
 	 * specified index) from this array if index lower than 0 or index
@@ -128,12 +129,12 @@ public interface StudentArrayOperation {
 	 *
 	 * @param index
 	 *            the index elements from should be removed
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void removeFromIndex(int index);
 
-	
+
 	/**
 	 * Removes all elements from the specified element (except the specified
 	 * element) from this array if student == null method should throw
@@ -141,12 +142,12 @@ public interface StudentArrayOperation {
 	 *
 	 * @param student
 	 *            the element from elements should be removed
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void removeFromElement(Student student);
 
-	
+
 	/**
 	 * Removes all elements to the specified index (except the element with the
 	 * specified index) from this array if index lower than 0 or index
@@ -154,12 +155,12 @@ public interface StudentArrayOperation {
 	 *
 	 * @param index
 	 *            the index elements to should be removed
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void removeToIndex(int index);
 
-	
+
 	/**
 	 * Removes all elements to the specified element (except the specified
 	 * element) from this array if student == null method should throw
@@ -167,11 +168,11 @@ public interface StudentArrayOperation {
 	 *
 	 * @param student
 	 *            the element to elements should be removed
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	void removeToElement(Student student);
-	
+
 
 	/**
 	 * Performs Bubble Sort to this array
@@ -179,98 +180,98 @@ public interface StudentArrayOperation {
 	 */
 	void bubbleSort();
 
-	
+
 	/**
 	 * Returns all students with birthday to the specified date if date == null,
 	 * method should throw IllegalArgumentException
-	 * 
+	 *
 	 * @param date
 	 *            the date that specify birthday to find students who was born
 	 *            before that date and in that date
-	 * 
+	 *
 	 * @return the array of students who was born before/in specified date
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	Student[] getByBirthDate(Date date);
 
-	
+
 	/**
 	 * Returns all students with birthday to the specified range of dates
 	 * including specified dates if firstDate == null or lastDate == null,
 	 * method should throw IllegalArgumentException
-	 * 
+	 *
 	 * @param firstDate
 	 *            the date that specify birthday to find students from
 	 * @param lastDate
 	 *            the date that specify birthday to find students to
-	 * 
+	 *
 	 * @return the array of students who was born in specified range of dates
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	Student[] getBetweenBirthDates(Date firstDate, Date lastDate);
 
-	
+
 	/**
 	 * Returns all students with birthday to the specified date and in several
 	 * days after the specified date if date == null, method should throw
 	 * IllegalArgumentException
-	 * 
+	 *
 	 * @param date
 	 *            the date that specify birthday to find students from
 	 * @param days
 	 *            the number of days that specify the end date
-	 * 
+	 *
 	 * @return the array of students who has birthday in specified date and
 	 *         several days after
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	Student[] getNearBirthDate(Date date, int days);
-	
+
 
 	/**
 	 * Find student at the specified position and calculate his age (total
 	 * years) if indexOfStudent == 0, method should throw
 	 * IllegalArgumentException
-	 * 
+	 *
 	 * @param indexOfStudent
 	 *            the index to find student and calculate age
-	 * 
+	 *
 	 * @return student full years
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	int getCurrentAgeByDate(int indexOfStudent);
 
-	
+
 	/**
 	 * Returns all students with age equal the specified age
-	 * 
+	 *
 	 * @param age the age to find students
-	 * 
+	 *
 	 * @return the array of students of the specified age
 	 */
 	Student[] getStudentsByAge(int age);
-	
+
 
 	/**
 	 * Finds the maximum average mark for all students in this array
 	 * and returns all students who has the maximum average mark
-	 * 
+	 *
 	 * @return the array of students who has maximum average mark
-	 * 
+	 *
 	 */
 	Student[] getStudentsWithMaxAvgMark();
 
-	
+
 	/**
 	 * Finds element which is the next to the specified element
 	 * if student == null, method should throw IllegalArgumentException
-	 * 
+	 *
 	 * @param student the element to which take the next element
-	 * 
+	 *
 	 * @throws IllegalArgumentException
 	 */
 	Student getNextStudent(Student student);
